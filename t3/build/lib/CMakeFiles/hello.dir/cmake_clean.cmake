41d6ae0a7603fae17894142e04f5c6cea9eed0af
chendong-roadstar <chendong@roadstar.ai>
2018-08-27 22:41:54 +0800

    initial commit

---
+++ t3/build/lib/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/hello.o"
+  "../../lib/libhello.pdb"
+  "../../lib/libhello.so"
+  "../../lib/libhello.so.1.2"
+  "../../lib/libhello.so.1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
